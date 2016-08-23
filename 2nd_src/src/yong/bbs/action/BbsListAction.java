612914a4a688de47efcee697e580c0ce2555ef0e
github school7341 <github school7341@naver.com>
2016-08-23 12:25:10 +0900

    2nd Commit

--- 2nd_src/src/yong/bbs/action/BbsListAction.java
+++ 2nd_src/src/yong/bbs/action/BbsListAction.java
@@ -1,22 +1,38 @@
 package yong.bbs.action;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import yong.action.CommandHandler;
-import yong.emp.model.EmpDAO;
-import yong.emp.model.EmpDTO;
+import java.util.*;
+import yong.bbs.*;
 
 public class BbsListAction implements CommandHandler {
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		BbsDAO dao=new BbsDAO();
 		
+		int total_cnt=dao.getTotalCnt();
+		int list_size=10;
+		int page_size=5;
+		
+		String cp_s=req.getParameter("cp");
+		if(cp_s==null||cp_s.equals("")){
+			cp_s="1";
+		}
+		int cp=Integer.parseInt(cp_s);
+		
+		String pageStr=yong.page.YongPageModuel.makePage(total_cnt, list_size, page_size, cp, "bbsList.yong");
+		req.setAttribute("pageStr",pageStr);
+		
+		ArrayList<BbsDTO> arr=dao.bbsList(cp,list_size);
+		req.setAttribute("lists", arr);
 		return "/jsp_bbs/jspList.jsp";
 	}
+
 }
